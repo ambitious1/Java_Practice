e23502b5985355148913bedd1cd6fb12a9397ba5
Travis D <travis.dacosta1@gmail.com>
2015-10-22 09:25:41 -0400

    variables.java

---
+++ variables.java
@@ -0,0 +1,33 @@
+public class variables {
+    
+    public static void main(String[] args) {
+        int myNumber = 88;
+        int myNumber2 = 2;
+        int total = myNumber + myNumber2;
+        System.out.print(myNumber);
+        System.out.print("+");
+        System.out.print(myNumber2);
+        System.out.print("=");
+        System.out.print(total);
+        
+        short myShort = 453;
+        long myLong = 3329;
+        
+        double myDouble = 1.2456;
+        float myFloat = 324.3f;
+
+        char myChar = 'y';
+        boolean myBoolean = true;
+
+        byte myByte = 127;
+
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+
+}
