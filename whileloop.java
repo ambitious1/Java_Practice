14f8e41b0d1c09737f71320d349804a8c864ec05
Travis D <travis.dacosta1@gmail.com>
2015-10-22 09:24:47 -0400

    whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int value = 0;
+        
+        while (value < 10)
+        {
+            System.out.println("Hello " + value);
+            value = value + 1;
+        }
+        
+    }
+}
